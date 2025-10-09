3622aa8ccff35e1dc3c6346342661434232838e8
ushpineva <ushpineva@ozon.ru>
2025-10-10 01:06:21 +0300

    .

--- frontend/src/components/OrganizationsPage.tsx
+++ frontend/src/components/OrganizationsPage.tsx
@@ -109,7 +109,7 @@ const OrganizationsPage: React.FC = () => {
   const handleDeleteByFullname = async (fullName: string) => {
     if (window.confirm(`Вы уверены, что хотите удалить организацию с полным именем "${fullName}"?`)) {
       try {
-        await deleteByFullname({ fullname: fullName }).unwrap();
+        await deleteByFullname({ body: {fullname: fullName} }).unwrap();
         toast.success('Организация успешно удалена');
         loadOrganizations();
       } catch (error: any) {
