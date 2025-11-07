2e5d5f5271d95fb3a0d6262fb4f1800e06b80ff7
ushpineva <ushpineva@ozon.ru>
2025-11-08 01:52:59 +0300

    add unleash toggle

--- backend/organizations_service/src/main/java/ru/itmo/soa/services/UnleashSubscriptionService.java
+++ backend/organizations_service/src/main/java/ru/itmo/soa/services/UnleashSubscriptionService.java
@@ -1,41 +1,45 @@
 package ru.itmo.soa.services;
 
 import io.getunleash.Unleash;
-import io.getunleash.event.UnleashEvent;
-import io.getunleash.event.UnleashSubscriber;
+import io.getunleash.event.ToggleEvaluated;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import ru.itmo.soa.datasource_configuration.ClientDatabase;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class UnleashSubscriptionService implements UnleashSubscriber {
+public class UnleashSubscriptionService {
 
     private final Unleash unleash;
     private final DatabaseService databaseService;
-    
-    @Value("${unleash.database.feature-flag:use-postgresql}")
+
+    @Value("${unleash.database.feature-flag:select-mysql-db}")
     private String databaseFeatureFlag;
 
-    @Override
-    public void on(UnleashEvent event) {
-        log.info("Unleash toggles updated, updating database type");
-        // При обновлении фичей обновляем тип БД
-        updateDatabaseType();
+    @EventListener
+    public void handleUnleashEvent(ToggleEvaluated event) {
+        if (databaseFeatureFlag.equals(event.getToggleName())) {
+            log.info("Database toggle changed, updating database type");
+            updateDatabaseType();
+        }
     }
 
-    /**
-     * Обновляет тип используемой БД на основе значения фичафлага
-     * Если фичафлаг включен, используется MySQL, иначе PostgreSQL
-     */
+//    @EventListener
+//    public void handleUnleashReady(UnleashReadyEvent event) {
+//        log.info("Unleash initialized, setting initial database type");
+//        updateDatabaseType(event.getUnleash());
+//    }
+
     private void updateDatabaseType() {
         try {
             boolean useMySQL = unleash.isEnabled(databaseFeatureFlag);
             ClientDatabase newDatabaseType = useMySQL ? ClientDatabase.MYSQL : ClientDatabase.POSTGRESQL;
             databaseService.setConfiguration(newDatabaseType);
+            log.info("Database type updated to: {}", newDatabaseType);
         } catch (Exception e) {
             log.error("Error updating database type based on feature flag '{}'", databaseFeatureFlag, e);
         }
